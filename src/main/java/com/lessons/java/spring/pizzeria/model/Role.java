4c4febb2531191d32d7e89e8ed5471593f8b9e00
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-21 13:57:31 +0200

    add: created model to method authentication and authorization

---
+++ src/main/java/com/lessons/java/spring/pizzeria/model/Role.java
@@ -0,0 +1,34 @@
+package com.lessons.java.spring.pizzeria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+//ENTITA' ROLE PER L'AUTENTICAZIONE
+@Entity
+public class Role {
+
+	@Id
+	private Integer id;
+	
+	@NotNull
+	private String name;
+
+	//GETTER - SETTER
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
